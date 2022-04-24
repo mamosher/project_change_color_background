dc60efadc5eb99246a4214edef8de73b7b259f3e
Molly M <98428419+mamosher@users.noreply.github.com>
2022-04-24 13:13:27 -0400

    Change color background JS

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
-//Choose a random color
-const button = document.querySelector('button')
-const body = document.querySelector('body')
-const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+const button = document.querySelector("button");
+const body = document.querySelector("body"); 
+const colors = ["blue", "black", "yellow", "green", "red", "pink", "blue"]; 
 
-body.style.backgroundColor = 'violet'
-button.addEventListener('click', changeBackground)
-
-function changeBackground(){
-const colorIndex= parseInt(Math.random()*colors.length)
-body.style.backgroundColor = colors[colorIndex]
-}
+body.style.backgroundColor = "fuscia";
+button.addEventListener("click", function(changeBackground){
+    const colorIndex = parseInt(Math.random() * colors.length)
+    body.style.backgroundColor = colors[colorIndex]
+})
